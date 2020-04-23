1a0e05e3b8aeaf8bd2fc0bb80fca1d0f375ed0c8
Alexander Levinski <36931085+Alisterion95@users.noreply.github.com>
2020-04-23 20:39:27 +0300

    Configure database connection

--- Reactivities.Api/Program.cs
+++ Reactivities.Api/Program.cs
@@ -1,5 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Reactivities.Persistence;
 
 namespace Reactivities.Api
 {
@@ -7,7 +12,25 @@ namespace Reactivities.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ReactivitiesDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured during migration.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Reactivities.Persistence/ReactivitiesDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Reactivities.Persistence
+{
+    public class ReactivitiesDbContext : DbContext
+    {
+        public ReactivitiesDbContext(DbContextOptions options) : base(options) { }
+    }
+}
