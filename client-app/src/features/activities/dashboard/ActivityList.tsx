b5f2880663b05efbb68f37c9272cdb4fd471466d
Alexander Levinski <36931085+Alisterion95@users.noreply.github.com>
2020-05-07 23:32:48 +0300

    Add MobX

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,49 +1,53 @@
-import React, { SyntheticEvent } from 'react'
-import { Item, Button, Label, Segment } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import React, { useContext } from "react";
+import { Item, Button, Label, Segment } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import ActivityStore from "../../../app/stores/activityStore";
 
-interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    submitting: boolean;
-    target: string;
-}
+const ActivityList: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    activitiesByDate,
+    selectActivity,
+    deleteActivity,
+    submitting,
+    target,
+  } = activityStore;
 
-export const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity, submitting, target }) => {
-    return (
-        <Segment clearing>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.name}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{`${activity.city}, ${activity.venue}`}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button 
-                                    floated="right" 
-                                    content="View" 
-                                    color="blue" 
-                                    onClick={() => selectActivity(activity.id)} 
-                                />
-                                <Button 
-                                    name={activity.id}
-                                    loading={target === activity.id && submitting}
-                                    floated="right" 
-                                    content="Delete" 
-                                    color="red" 
-                                    onClick={(e) => deleteActivity(e, activity.id)} 
-                                />
-                                <Label basic content={activity.category}></Label>
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment >
-    )
-}
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {activitiesByDate.map((activity) => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as="a">{activity.name}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>{`${activity.city}, ${activity.venue}`}</div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  floated="right"
+                  content="View"
+                  color="blue"
+                  onClick={() => selectActivity(activity.id)}
+                />
+                <Button
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                  onClick={(e) => deleteActivity(e, activity.id)}
+                />
+                <Label basic content={activity.category}></Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+};
+
+export default observer(ActivityList);
