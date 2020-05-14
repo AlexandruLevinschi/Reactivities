ae8852468887988ed3a5752d706911d597ebe78f
Alexander Levinski <36931085+Alisterion95@users.noreply.github.com>
2020-05-14 22:44:51 +0300

    Client side photo uploading

--- client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { List, Image, Popup } from "semantic-ui-react";
 import { IAttendee } from "../../../app/models/activity";
+import { Link } from "react-router-dom";
 
 interface IProps {
   attendees: IAttendee[];
@@ -15,7 +16,7 @@ const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
             size="mini"
             header={attendee.displayName}
             trigger={
-              <Image
+              <Image as={Link} to={`/profile/${attendee.username}`}
                 size="mini"
                 circular
                 src={attendee.image || "/assets/user.png"}
